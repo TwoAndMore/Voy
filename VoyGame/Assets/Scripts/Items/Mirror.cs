0ea7368d2cae0feecf17e8e8851675c30f1bad7e
BenderCity <65117662+BenderCity@users.noreply.github.com>
2022-01-24 04:36:00 +0200

    Mirror  Particle

--- VoyGame/Assets/Scripts/Items/Mirror.cs
+++ VoyGame/Assets/Scripts/Items/Mirror.cs
@@ -5,19 +5,32 @@ public class Mirror : MonoBehaviour
 {
     private const KeyCode ACTIVATECODE = KeyCode.E;
     private const string DIMENSIONTAG = "AnotherDimension";
+
+    [SerializeField] private AudioClip _delaySound;
+    [SerializeField] private AudioClip _dimensionSound;
+    [SerializeField] private AudioClip _goBackSound;
+    [SerializeField] private AudioClip _reloadedSound;
+    [SerializeField] private GameObject _particles;
+    [SerializeField] private Material _glassMaterial;
     
     private Vector3 _startPosition;
     private Transform _teleportPosition;
+    private AudioSource _audioSource;
+    private Color32 _activeColor = new Color32(255,0,0,200);
+    private Color32 _reloadColor = new Color32(70, 70, 70, 225);
+    private Color32 _mainColor;
     private bool _isReloading;
     private float _reloadTime = 5f;
-    private float _delayTIme = 1f;
-    private float _dimensionTime = 3f;
+    private float _delayTIme = 4f;
+    private float _dimensionTime = 10f;
     
     public GameObject player;
     
     private void Awake()
     {
+        _mainColor = _glassMaterial.color;
         _teleportPosition = GameObject.FindWithTag(DIMENSIONTAG).transform;
+        _audioSource = GetComponent<AudioSource>();
     }
     
     private void Update()
@@ -29,6 +42,7 @@ public class Mirror : MonoBehaviour
     private void Teleport()
     {
         _startPosition = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+        _audioSource.PlayOneShot(_dimensionSound);
         ChangePlayerPosition(_teleportPosition);
         StartCoroutine(Reload());
         StartCoroutine(TimeToGoBack());
@@ -41,9 +55,10 @@ public class Mirror : MonoBehaviour
         ChangePlayerPosition(oldPosition);
     }
 
-    private void VisualEffects()
+    private void VisualEffects(bool state, Color32 color)
     {
-        
+        _particles.SetActive(state);
+        _glassMaterial.color = color;
     }
 
     private void ChangePlayerPosition(Transform newPosition)
@@ -55,20 +70,29 @@ public class Mirror : MonoBehaviour
 
     private IEnumerator Delay()
     {
+        VisualEffects(true, _activeColor);
+        _audioSource.PlayOneShot(_delaySound);
         _isReloading = true;
         yield return new WaitForSeconds(_delayTIme);
         Teleport();
+        VisualEffects(false, _mainColor);
     }
 
     private IEnumerator Reload()
     {
         yield return new WaitForSeconds(_reloadTime + _dimensionTime);
+        VisualEffects(false, _mainColor);
+        _audioSource.PlayOneShot(_reloadedSound);
         _isReloading = false;
     }
 
     private IEnumerator TimeToGoBack()
     {
-        yield return new WaitForSeconds(_dimensionTime);
+        yield return new WaitForSeconds(_dimensionTime - _goBackSound.length);
+        _audioSource.PlayOneShot(_goBackSound);
+        VisualEffects(true, _activeColor);
+        yield return new WaitForSeconds(_goBackSound.length);
         TeleportBack();
+        VisualEffects(false, _reloadColor);
     }
 }
